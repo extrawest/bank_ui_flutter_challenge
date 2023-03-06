3baa43d277a5fa0219065ead284334ceb3c0e33d
Oleksii <oleksii.melnychenko@extrawest.com>
2023-03-06 17:48:01 +0200

    added card variety :)

--- lib/providers/fake_credit_card_provider.dart
+++ lib/providers/fake_credit_card_provider.dart
@@ -7,14 +7,14 @@ final fakeCreditCardProvider = Provider((ref) => [
     card: CreditCardModel(
       cardNumber: 1234567890123456,
       balance: 5750.20,
-      expiryDate: DateTime.now(),
+      expiryDate: DateTime.now().add(const Duration(days: 28)),
     ),
   ),
   CreditCard(
     card: CreditCardModel(
-      cardNumber: 1234567890123456,
-      balance: 5750.20,
-      expiryDate: DateTime.now(),
+      cardNumber: 1234567890128127,
+      balance: 10985.84,
+      expiryDate: DateTime.now().add(const Duration(days: 35)),
     ),
   ),
 ]);
